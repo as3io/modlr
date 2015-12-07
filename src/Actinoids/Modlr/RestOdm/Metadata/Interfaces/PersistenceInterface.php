9f53ff5eaaee87c6283e3699c2c8ab40d513f0db
Jacob Bare <jacob.bare@cygnus.com>
2015-12-07 14:27:47 -0600

    Create persistence metadata interface

---
+++ src/Actinoids/Modlr/RestOdm/Metadata/Interfaces/PersistenceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Actinoids\Modlr\RestOdm\Metadata\Interfaces;
+
+/**
+ * Defines the persistence metadata for an entity (e.g. a database object).
+ * Should be loaded using the MetadataFactory, not instantiated directly.
+ * Contains information about the database schema, such as db/collection/table names, indexes, etc.
+ * Each implementing class must define it's own merging criteria, and handle it's own properties/methods.
+ *
+ * @author Jacob Bare <jacob.bare@gmail.com>
+ */
+interface PersistenceInterface extends MergeableInterface
+{
+}
