5c85263f0e3af1a7fdc9830f5ec1cfa509b9c6a2
Josh Worden <solocommand@gmail.com>
2016-04-04 09:49:54 -0500

    Dispatch postLoad metadata event

---
+++ src/Metadata/Events.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace As3\Modlr\Metadata;
+
+/**
+ * Store event name constants.
+ *
+ * @author Josh Worden <solocommand@gmail.com>
+ */
+class Events
+{
+    const preLoad   = 'preLoad';
+    const postLoad  = 'postLoad';
+}
