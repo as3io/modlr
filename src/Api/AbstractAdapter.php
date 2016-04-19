417add0af978f5f06fb20cc1fac77810eb610ed0
Jacob Bare <jacob.bare@cygnus.com>
2016-04-19 10:45:06 -0500

    Fix wrong collection namespace

--- src/Api/AbstractAdapter.php
+++ src/Api/AbstractAdapter.php
@@ -4,7 +4,7 @@ namespace As3\Modlr\Api;
 
 use As3\Modlr\Exception\HttpExceptionInterface;
 use As3\Modlr\Metadata\EntityMetadata;
-use As3\Modlr\Models\Collection;
+use As3\Modlr\Models\Collections\Collection;
 use As3\Modlr\Models\Model;
 use As3\Modlr\Rest;
 use As3\Modlr\Store\Store;
