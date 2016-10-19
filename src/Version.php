d4760c81fda36a68e55ad89e9383ed3bac684f90
Josh Worden <jworden@southcomm.com>
2016-10-19 10:20:14 -0500

    Prep versions for hotfix

--- src/Version.php
+++ src/Version.php
@@ -9,10 +9,10 @@ namespace As3\Modlr;
  */
 class Version
 {
-    const VERSION = '0.3.16';
-    const ID = 3016;
+    const VERSION = '0.3.17';
+    const ID = 3017;
     const MAJOR = 0;
     const MINOR = 3;
-    const PATCH = 16;
+    const PATCH = 17;
     const EXTRA = '';
 }
