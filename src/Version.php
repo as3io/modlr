3092fd2994cc7c66bb69c79f6e88ed16f4e800c8
Jacob Bare <jacob@limit0.io>
2017-02-26 18:01:23 -0600

    Bump version

--- src/Version.php
+++ src/Version.php
@@ -9,10 +9,10 @@ namespace As3\Modlr;
  */
 class Version
 {
-    const VERSION = '0.3.19';
-    const ID = 3019;
+    const VERSION = '0.3.20';
+    const ID = 3020;
     const MAJOR = 0;
     const MINOR = 3;
-    const PATCH = 19;
+    const PATCH = 20;
     const EXTRA = '';
 }
